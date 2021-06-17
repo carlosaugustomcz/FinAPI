60326cb939bc7ecaede035b26a00dba90aaf5299
carlos.augustomcz <carlos.augustomcz@gmail.com>
2021-06-17 08:06:37 -0300

    complemento criar conta

--- scr/index.js
+++ scr/index.js
@@ -1,8 +1,28 @@
 const express = require ("express");
+const {v4: uuidv4} = require ("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+const Customers = [];
+
+app.post("/account", (request, response) => {
+
+ const {cpf, name} = request.body;
+
+ const id = uuidv4;
+
+ Customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+ });
+
+ response.status(201).send("Conta Criada");
+ console.log(Customers);
+});
+
 app.listen(3333);
 console.log('Server Start');
-
-
